c39c2a6540d598827eb9eb484d275f1fd2ee5040
Pathi Anil <madhuanil25@gmail.com>
2023-10-18 08:31:16 +0530

    Shell Topics

--- 02-print.sh
+++ 02-print.sh
@@ -3,8 +3,8 @@ echo Hello World
 echo "**** Hello World ****"
 
 echo -e "\e[31mMessage{red}\e[0m"
-echo -e "\e[32mMessage{red}\e[0m"
-echo -e "\e[33mMessage{red}\e[0m"
-echo -e "\e[34mMessage{red}\e[0m"
-echo -e "\e[35mMessage{red}\e[0m"
+echo -e "\e[32mMessage{Green}\e[0m"
+echo -e "\e[33mMessage{Yellow}\e[0m"
+echo -e "\e[34mMessage{Blue}\e[0m"
+echo -e "\e[35mMessage(red)\e[0m"
 echo -e "\e[36mMessage{red}\e[0m"
