a1e865bcd2668f4b4e411c2167c19a21eb8daeff
Pathi Anil <madhuanil25@gmail.com>
2023-10-18 08:29:57 +0530

    Shell Topics

--- 02-print.sh
+++ 02-print.sh
@@ -2,4 +2,9 @@ echo Hello World
 
 echo "**** Hello World ****"
 
-echo -e "\e[31mMessage\e[0m"
+echo -e "\e[31mMessage{red}\e[0m"
+echo -e "\e[32mMessage{red}\e[0m"
+echo -e "\e[33mMessage{red}\e[0m"
+echo -e "\e[34mMessage{red}\e[0m"
+echo -e "\e[35mMessage{red}\e[0m"
+echo -e "\e[36mMessage{red}\e[0m"
